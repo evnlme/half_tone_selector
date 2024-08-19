f4082c9c2d5eb5f0fe5e1126c0ca93beac13655e
Evan Lee <112362737+evnlme@users.noreply.github.com>
2024-08-18 18:47:50 -0700

    Remove function calls in const expressions

--- half_tone_selector/shaders/color.glsl
+++ half_tone_selector/shaders/color.glsl
@@ -1,24 +1,32 @@
-const mat3 xy_to_xyz = mat3(
-    1.0, 0.0,-1.0,
-    0.0, 1.0,-1.0,
-    0.0, 0.0, 1.0
-);
-const mat3 primaries_xy = mat3(
-    0.64, 0.33, 1.0,
-    0.30, 0.60, 1.0,
-    0.15, 0.06, 1.0
-);
-const vec3 whitepoint_xy = vec3(0.3127, 0.3290, 1.0);
-const mat3 primaries_xyz = transpose(xy_to_xyz * primaries_xy);
-const vec3 whitepoint_xyz = xy_to_xyz * whitepoint_xy;
-const vec3 coeff = whitepoint_xyz * inverse(primaries_xyz) / whitepoint_xyz.y;
-const mat3 coeff_mat = mat3(
-    coeff.x, 0.0, 0.0,
-    0.0, coeff.y, 0.0,
-    0.0, 0.0, coeff.z
-);
-const mat3 rgb_to_xyz_mat = coeff_mat * primaries_xyz;
-const mat3 xyz_to_rgb_mat = inverse(rgb_to_xyz_mat);
+// const mat3 xy_to_xyz = mat3(
+//     1.0, 0.0,-1.0,
+//     0.0, 1.0,-1.0,
+//     0.0, 0.0, 1.0
+// );
+// const mat3 primaries_xy = mat3(
+//     0.64, 0.33, 1.0,
+//     0.30, 0.60, 1.0,
+//     0.15, 0.06, 1.0
+// );
+// const vec3 whitepoint_xy = vec3(0.3127, 0.3290, 1.0);
+// const mat3 primaries_xyz = transpose(xy_to_xyz * primaries_xy);
+// const vec3 whitepoint_xyz = xy_to_xyz * whitepoint_xy;
+// const vec3 coeff = whitepoint_xyz * inverse(primaries_xyz) / whitepoint_xyz.y;
+// const mat3 coeff_mat = mat3(
+//     coeff.x, 0.0, 0.0,
+//     0.0, coeff.y, 0.0,
+//     0.0, 0.0, coeff.z
+// );
+const mat3 rgb_to_xyz_mat = mat3(
+    0.41239079926595923, 0.35758433938387796, 0.18048078840183432,
+    0.21263900587151022, 0.7151686787677559, 0.07219231536073373,
+    0.019330818715591856, 0.11919477979462609, 0.9505321522496608
+); // coeff_mat * primaries_xyz;
+const mat3 xyz_to_rgb_mat = mat3(
+    3.240969941904524, -1.5373831775700944, -0.4986107602930036,
+    -0.9692436362808798, 1.8759675015077206, 0.041555057407175626,
+    0.05563007969699364, -0.20397695888897668, 1.0569715142428784
+); // inverse(rgb_to_xyz_mat);
 
 vec3 rgb_to_xyz(vec3 rgb) {
     return rgb * rgb_to_xyz_mat;
@@ -32,13 +40,21 @@ const mat3 xyz_to_lms_mat = mat3(
     0.0329845436, 0.9293118715, 0.0361456387,
     0.0482003018, 0.2643662691, 0.6338517070
 );
-const mat3 lms_to_xyz_mat = inverse(xyz_to_lms_mat);
+const mat3 lms_to_xyz_mat = mat3(
+    1.2270138511035211, -0.5577999806518221, 0.2812561489664678,
+    -0.040580178423280586, 1.11225686961683, -0.07167667866560119,
+    -0.0763812845057069, -0.4214819784180127, 1.5861632204407947
+); // inverse(xyz_to_lms_mat);
 const mat3 lms_to_oklab_mat = mat3(
     0.2104542553, 0.7936177850,-0.0040720468,
     1.9779984951,-2.4285922050, 0.4505937099,
     0.0259040371, 0.7827717662,-0.8086757660
 );
-const mat3 oklab_to_lms_mat = inverse(lms_to_oklab_mat);
+const mat3 oklab_to_lms_mat = mat3(
+    0.9999999984505197, 0.39633779217376786, 0.21580375806075883,
+    1.0000000088817607, -0.10556134232365633, -0.0638541747717059,
+    1.0000000546724108, -0.08948418209496577, -1.2914855378640917
+); // inverse(lms_to_oklab_mat);
 
 vec3 xyz_to_oklab(vec3 xyz) {
     return pow(xyz * xyz_to_lms_mat, vec3(1.0/3.0)) * lms_to_oklab_mat;
